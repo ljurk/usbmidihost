53e029db755193b37cace8a0d107b7aef795f974
Lukas Jurk <lukas.jurk@tu-dresden.de>
2021-11-21 14:13:48 +0100

    update code to work with new 1,44 inch display

--- roles/usbmidihost/files/display.py
+++ roles/usbmidihost/files/display.py
@@ -1,130 +1,314 @@
-#!/usr/bin/python
-#--------------------------------------
-#    ___  ___  _ ____
-#   / _ \/ _ \(_) __/__  __ __
-#  / , _/ ___/ /\ \/ _ \/ // /
-# /_/|_/_/  /_/___/ .__/\_, /
-#                /_/   /___/
-#
-#  lcd_i2c.py
-#  LCD test script using I2C backpack.
-#  Supports 16x2 and 20x4 screens.
-#
-# Author : Matt Hawkins
-# Date   : 20/09/2015
-#
-# http://www.raspberrypi-spy.co.uk/
-#
-# Copyright 2015 Matt Hawkins
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#--------------------------------------
-import smbus
-import sys
+ # -*- coding:UTF-8 -*-
+ ##
+ # | file      	:	LCD_1IN44.py
+ # |	version		:	V2.0
+ # | date		:	2018-07-16
+ # | function	:	On the ST7735S chip driver and clear screen, drawing lines, drawing, writing
+ #					and other functions to achieve
+ #
+ # Permission is hereby granted, free of charge, to any person obtaining a copy
+ # of this software and associated documnetation files (the "Software"), to deal
+ # in the Software without restriction, including without limitation the rights
+ # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ # copies of the Software, and to permit persons to  whom the Software is
+ # furished to do so, subject to the following conditions:
+ #
+ # The above copyright notice and this permission notice shall be included in
+ # all copies or substantial portions of the Software.
+ #
+ # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ # FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ # LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ # THE SOFTWARE.
+ #
+
+import LCD_Config
+import RPi.GPIO as GPIO
 import time
+import numpy as np
 
-# Define some device parameters
-I2C_ADDR  = 0x27 # I2C device address
-LCD_WIDTH = 16   # Maximum characters per line
+LCD_1IN44 = 1
+LCD_1IN8 = 0
+if LCD_1IN44 == 1:
+	LCD_WIDTH  = 128  #LCD width
+	LCD_HEIGHT = 128 #LCD height
+	LCD_X = 2
+	LCD_Y = 1
+if LCD_1IN8 == 1:
+	LCD_WIDTH  = 160
+	LCD_HEIGHT = 128
+	LCD_X = 1
+	LCD_Y = 2
 
-# Define some device constants
-LCD_CHR = 1 # Mode - Sending data
-LCD_CMD = 0 # Mode - Sending command
+LCD_X_MAXPIXEL = 132  #LCD width maximum memory
+LCD_Y_MAXPIXEL = 162  #LCD height maximum memory
 
-lines = [0x80, 0xC0, 0x94, 0xD4]
-LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
-LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
-LCD_LINE_3 = 0x94 # LCD RAM address for the 3rd line
-LCD_LINE_4 = 0xD4 # LCD RAM address for the 4th line
+#scanning method
+L2R_U2D = 1
+L2R_D2U = 2
+R2L_U2D = 3
+R2L_D2U = 4
+U2D_L2R = 5
+U2D_R2L = 6
+D2U_L2R = 7
+D2U_R2L = 8
+SCAN_DIR_DFT = U2D_R2L
 
-LCD_BACKLIGHT  = 0x08  # On
-#LCD_BACKLIGHT = 0x00  # Off
 
-ENABLE = 0b00000100 # Enable bit
+class LCD:
+	def __init__(self):
+		self.width = LCD_WIDTH
+		self.height = LCD_HEIGHT
+		self.LCD_Scan_Dir = SCAN_DIR_DFT
+		self.LCD_X_Adjust = LCD_X
+		self.LCD_Y_Adjust = LCD_Y
 
-# Timing constants
-E_PULSE = 0.0005
-E_DELAY = 0.0005
+	"""    Hardware reset     """
+	def  LCD_Reset(self):
+		GPIO.output(LCD_Config.LCD_RST_PIN, GPIO.HIGH)
+		LCD_Config.Driver_Delay_ms(100)
+		GPIO.output(LCD_Config.LCD_RST_PIN, GPIO.LOW)
+		LCD_Config.Driver_Delay_ms(100)
+		GPIO.output(LCD_Config.LCD_RST_PIN, GPIO.HIGH)
+		LCD_Config.Driver_Delay_ms(100)
 
-class Display:
+	"""    Write register address and data     """
+	def  LCD_WriteReg(self, Reg):
+		GPIO.output(LCD_Config.LCD_DC_PIN, GPIO.LOW)
+		LCD_Config.SPI_Write_Byte([Reg])
 
-    def __init__(self):
-        #Open I2C interface
-        #bus = smbus.SMBus(0)  # Rev 1 Pi uses 0
-        self.bus = smbus.SMBus(1) # Rev 2 Pi uses 1
+	def LCD_WriteData_8bit(self, Data):
+		GPIO.output(LCD_Config.LCD_DC_PIN, GPIO.HIGH)
+		LCD_Config.SPI_Write_Byte([Data])
 
-        # Initialise display
-        self.lcd_byte(0x33,LCD_CMD) # 110011 Initialise
-        self.lcd_byte(0x32,LCD_CMD) # 110010 Initialise
-        self.lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
-        self.lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
-        self.lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
-        self.lcd_byte(0x01,LCD_CMD) # 000001 Clear display
-        time.sleep(E_DELAY)
+	def LCD_WriteData_NLen16Bit(self, Data, DataLen):
+		GPIO.output(LCD_Config.LCD_DC_PIN, GPIO.HIGH)
+		for i in range(0, DataLen):
+			LCD_Config.SPI_Write_Byte([Data >> 8])
+			LCD_Config.SPI_Write_Byte([Data & 0xff])
 
-    def lcd_byte(self, bits, mode):
-      # Send byte to data pins
-      # bits = the data
-      # mode = 1 for data
-      #        0 for command
+	"""    Common register initialization    """
+	def LCD_InitReg(self):
+		#ST7735R Frame Rate
+		self.LCD_WriteReg(0xB1)
+		self.LCD_WriteData_8bit(0x01)
+		self.LCD_WriteData_8bit(0x2C)
+		self.LCD_WriteData_8bit(0x2D)
 
-      bits_high = mode | (bits & 0xF0) | LCD_BACKLIGHT
-      bits_low = mode | ((bits<<4) & 0xF0) | LCD_BACKLIGHT
+		self.LCD_WriteReg(0xB2)
+		self.LCD_WriteData_8bit(0x01)
+		self.LCD_WriteData_8bit(0x2C)
+		self.LCD_WriteData_8bit(0x2D)
 
-      # High bits
-      self.bus.write_byte(I2C_ADDR, bits_high)
-      self.lcd_toggle_enable(bits_high)
+		self.LCD_WriteReg(0xB3)
+		self.LCD_WriteData_8bit(0x01)
+		self.LCD_WriteData_8bit(0x2C)
+		self.LCD_WriteData_8bit(0x2D)
+		self.LCD_WriteData_8bit(0x01)
+		self.LCD_WriteData_8bit(0x2C)
+		self.LCD_WriteData_8bit(0x2D)
 
-      # Low bits
-      self.bus.write_byte(I2C_ADDR, bits_low)
-      self.lcd_toggle_enable(bits_low)
+		#Column inversion
+		self.LCD_WriteReg(0xB4)
+		self.LCD_WriteData_8bit(0x07)
 
-    def lcd_toggle_enable(self, bits):
-      # Toggle enable
-      time.sleep(E_DELAY)
-      self.bus.write_byte(I2C_ADDR, (bits | ENABLE))
-      time.sleep(E_PULSE)
-      self.bus.write_byte(I2C_ADDR,(bits & ~ENABLE))
-      time.sleep(E_DELAY)
+		#ST7735R Power Sequence
+		self.LCD_WriteReg(0xC0)
+		self.LCD_WriteData_8bit(0xA2)
+		self.LCD_WriteData_8bit(0x02)
+		self.LCD_WriteData_8bit(0x84)
+		self.LCD_WriteReg(0xC1)
+		self.LCD_WriteData_8bit(0xC5)
 
-    def lcd_string(self, message,line):
-      # Send string to display
+		self.LCD_WriteReg(0xC2)
+		self.LCD_WriteData_8bit(0x0A)
+		self.LCD_WriteData_8bit(0x00)
 
-      message = message.ljust(LCD_WIDTH," ")
+		self.LCD_WriteReg(0xC3)
+		self.LCD_WriteData_8bit(0x8A)
+		self.LCD_WriteData_8bit(0x2A)
+		self.LCD_WriteReg(0xC4)
+		self.LCD_WriteData_8bit(0x8A)
+		self.LCD_WriteData_8bit(0xEE)
 
-      self.lcd_byte(lines[line], LCD_CMD)
+		self.LCD_WriteReg(0xC5)#VCOM
+		self.LCD_WriteData_8bit(0x0E)
 
-      for i in range(LCD_WIDTH):
-        self.lcd_byte(ord(message[i]),LCD_CHR)
+		#ST7735R Gamma Sequence
+		self.LCD_WriteReg(0xe0)
+		self.LCD_WriteData_8bit(0x0f)
+		self.LCD_WriteData_8bit(0x1a)
+		self.LCD_WriteData_8bit(0x0f)
+		self.LCD_WriteData_8bit(0x18)
+		self.LCD_WriteData_8bit(0x2f)
+		self.LCD_WriteData_8bit(0x28)
+		self.LCD_WriteData_8bit(0x20)
+		self.LCD_WriteData_8bit(0x22)
+		self.LCD_WriteData_8bit(0x1f)
+		self.LCD_WriteData_8bit(0x1b)
+		self.LCD_WriteData_8bit(0x23)
+		self.LCD_WriteData_8bit(0x37)
+		self.LCD_WriteData_8bit(0x00)
+		self.LCD_WriteData_8bit(0x07)
+		self.LCD_WriteData_8bit(0x02)
+		self.LCD_WriteData_8bit(0x10)
 
-def main():
-  # Main program block
+		self.LCD_WriteReg(0xe1)
+		self.LCD_WriteData_8bit(0x0f)
+		self.LCD_WriteData_8bit(0x1b)
+		self.LCD_WriteData_8bit(0x0f)
+		self.LCD_WriteData_8bit(0x17)
+		self.LCD_WriteData_8bit(0x33)
+		self.LCD_WriteData_8bit(0x2c)
+		self.LCD_WriteData_8bit(0x29)
+		self.LCD_WriteData_8bit(0x2e)
+		self.LCD_WriteData_8bit(0x30)
+		self.LCD_WriteData_8bit(0x30)
+		self.LCD_WriteData_8bit(0x39)
+		self.LCD_WriteData_8bit(0x3f)
+		self.LCD_WriteData_8bit(0x00)
+		self.LCD_WriteData_8bit(0x07)
+		self.LCD_WriteData_8bit(0x03)
+		self.LCD_WriteData_8bit(0x10)
 
-  # Initialise display
-  lcd = display()
-  #lcd_init()
-  for y in range(0, len(sys.argv)-1):
-      print(sys.argv[y+1])
-      lcd.lcd_string(sys.argv[y+1], lines[y])
+		#Enable test command
+		self.LCD_WriteReg(0xF0)
+		self.LCD_WriteData_8bit(0x01)
 
-if __name__ == '__main__':
+		#Disable ram power save mode
+		self.LCD_WriteReg(0xF6)
+		self.LCD_WriteData_8bit(0x00)
 
-  try:
-    main()
-  except KeyboardInterrupt:
-    pass
-  finally:
-    print("finished")
-    #lcd_byte(0x01, LCD_CMD)
+		#65k mode
+		self.LCD_WriteReg(0x3A)
+		self.LCD_WriteData_8bit(0x05)
+
+	#********************************************************************************
+	#function:	Set the display scan and color transfer modes
+	#parameter:
+	#		Scan_dir   :   Scan direction
+	#		Colorchose :   RGB or GBR color format
+	#********************************************************************************
+	def LCD_SetGramScanWay(self, Scan_dir):
+		#Get the screen scan direction
+		self.LCD_Scan_Dir = Scan_dir
+
+		#Get GRAM and LCD width and height
+		if (Scan_dir == L2R_U2D) or (Scan_dir == L2R_D2U) or (Scan_dir == R2L_U2D) or (Scan_dir == R2L_D2U) :
+			self.width	= LCD_HEIGHT
+			self.height 	= LCD_WIDTH
+			if Scan_dir == L2R_U2D:
+				MemoryAccessReg_Data = 0X00 | 0x00
+			elif Scan_dir == L2R_D2U:
+				MemoryAccessReg_Data = 0X00 | 0x80
+			elif Scan_dir == R2L_U2D:
+				MemoryAccessReg_Data = 0x40 | 0x00
+			else:		#R2L_D2U:
+				MemoryAccessReg_Data = 0x40 | 0x80
+		else:
+			self.width	= LCD_WIDTH
+			self.height 	= LCD_HEIGHT
+			if Scan_dir == U2D_L2R:
+				MemoryAccessReg_Data = 0X00 | 0x00 | 0x20
+			elif Scan_dir == U2D_R2L:
+				MemoryAccessReg_Data = 0X00 | 0x40 | 0x20
+			elif Scan_dir == D2U_L2R:
+				MemoryAccessReg_Data = 0x80 | 0x00 | 0x20
+			else:		#R2L_D2U
+				MemoryAccessReg_Data = 0x40 | 0x80 | 0x20
+
+		#please set (MemoryAccessReg_Data & 0x10) != 1
+		if (MemoryAccessReg_Data & 0x10) != 1:
+			self.LCD_X_Adjust = LCD_Y
+			self.LCD_Y_Adjust = LCD_X
+		else:
+			self.LCD_X_Adjust = LCD_X
+			self.LCD_Y_Adjust = LCD_Y
+
+		# Set the read / write scan direction of the frame memory
+		self.LCD_WriteReg(0x36)		#MX, MY, RGB mode
+		if LCD_1IN44 == 1:
+			self.LCD_WriteData_8bit( MemoryAccessReg_Data | 0x08)	#0x08 set RGB
+		else:
+			self.LCD_WriteData_8bit( MemoryAccessReg_Data & 0xf7)	#RGB color filter panel
+
+	#/********************************************************************************
+	#function:
+	#			initialization
+	#********************************************************************************/
+	def LCD_Init(self, Lcd_ScanDir):
+		if (LCD_Config.GPIO_Init() != 0):
+			return -1
+
+		#Turn on the backlight
+		GPIO.output(LCD_Config.LCD_BL_PIN,GPIO.HIGH)
+
+		#Hardware reset
+		self.LCD_Reset()
+
+		#Set the initialization register
+		self.LCD_InitReg()
+
+		#Set the display scan and color transfer modes
+		self.LCD_SetGramScanWay(Lcd_ScanDir)
+		LCD_Config.Driver_Delay_ms(200)
+
+		#sleep out
+		self.LCD_WriteReg(0x11)
+		LCD_Config.Driver_Delay_ms(120)
+
+		#Turn on the LCD display
+		self.LCD_WriteReg(0x29)
+
+	#/********************************************************************************
+	#function:	Sets the start position and size of the display area
+	#parameter:
+	#	Xstart 	:   X direction Start coordinates
+	#	Ystart  :   Y direction Start coordinates
+	#	Xend    :   X direction end coordinates
+	#	Yend    :   Y direction end coordinates
+	#********************************************************************************/
+	def LCD_SetWindows(self, Xstart, Ystart, Xend, Yend):
+		#set the X coordinates
+		self.LCD_WriteReg(0x2A)
+		self.LCD_WriteData_8bit(0x00)
+		self.LCD_WriteData_8bit((Xstart & 0xff) + self.LCD_X_Adjust)
+		self.LCD_WriteData_8bit(0x00)
+		self.LCD_WriteData_8bit(((Xend - 1) & 0xff) + self.LCD_X_Adjust)
+
+		#set the Y coordinates
+		self.LCD_WriteReg (0x2B)
+		self.LCD_WriteData_8bit(0x00)
+		self.LCD_WriteData_8bit((Ystart & 0xff) + self.LCD_Y_Adjust)
+		self.LCD_WriteData_8bit(0x00)
+		self.LCD_WriteData_8bit(((Yend - 1) & 0xff )+ self.LCD_Y_Adjust)
+
+		self.LCD_WriteReg(0x2C)
+
+	def LCD_Clear(self):
+		#hello
+		_buffer = [0xff]*(self.width * self.height * 2)
+		self.LCD_SetWindows(0, 0, self.width, self.height)
+		GPIO.output(LCD_Config.LCD_DC_PIN, GPIO.HIGH)
+		for i in range(0,len(_buffer),4096):
+			LCD_Config.SPI_Write_Byte(_buffer[i:i+4096])
+
+	def LCD_ShowImage(self,Image,Xstart,Ystart):
+		if (Image == None):
+			return
+		imwidth, imheight = Image.size
+		if imwidth != self.width or imheight != self.height:
+			raise ValueError('Image must be same dimensions as display \
+				({0}x{1}).' .format(self.width, self.height))
+		img = np.asarray(Image)
+		pix = np.zeros((self.width,self.height,2), dtype = np.uint8)
+		pix[...,[0]] = np.add(np.bitwise_and(img[...,[0]],0xF8),np.right_shift(img[...,[1]],5))
+		pix[...,[1]] = np.add(np.bitwise_and(np.left_shift(img[...,[1]],3),0xE0),np.right_shift(img[...,[2]],3))
+		pix = pix.flatten().tolist()
+		self.LCD_SetWindows(0, 0, self.width , self.height)
+		GPIO.output(LCD_Config.LCD_DC_PIN, GPIO.HIGH)
+		for i in range(0,len(pix),4096):
+			LCD_Config.SPI_Write_Byte(pix[i:i+4096])
--- roles/usbmidihost/files/main.py
+++ roles/usbmidihost/files/main.py
@@ -1,32 +1,254 @@
 #!/usr/bin/python
+
 import re
+from datetime import timedelta
+import time
 import subprocess
-import display
 import socket
+import math
+
+import psutil
+
+import RPi.GPIO as GPIO
+
+from PIL import Image, ImageDraw, ImageFont
+import display
+import LCD_Config
+
+# font
+fontpath = '/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf'
+font = ImageFont.truetype(fontpath, 10)
+
+KEY_UP_PIN = 19
+KEY_DOWN_PIN = 6
+KEY_LEFT_PIN = 5
+KEY_RIGHT_PIN = 26
+KEY_PRESS_PIN = 13
+KEY1_PIN = 21
+KEY2_PIN = 20
+KEY3_PIN = 16
+LEFT = True
+RIGHT = False
 
 
-def getDeviceList():
-    command = 'aconnect -i -l'
-    result = subprocess.run(command.split(' '), stdout=subprocess.PIPE)
-    # create a list for each line in the command output and remove lines containing 'Through'
-    devices = [i for i in result.stdout.decode('utf-8').split('\n') if 'Through' not in i]
-    # find id and name for each device
-    deviceDict = []
-    for dev in devices:
-        match = re.match("client (\d*)\: '(.*)'", dev)
-        if match:
-            deviceDict.append({'id': match.group(1), 'name': match.group(2)})
-    return deviceDict
+
+class usbMidiHost():
+    devices = []
+    currentTime = time.time()
+    interval = 5
+
+    def __init__(self):
+        pass
+
+    def getDeviceList(self):
+        if time.time() - self.currentTime < self.interval and self.devices:
+            return self.devices
+        self.currentTime = time.time()
+
+        print(time.time() - self.currentTime)
+        command = 'aconnect -i -l'
+        result = subprocess.run(command.split(' '), stdout=subprocess.PIPE)
+        # create a list for each line in the command output and remove lines containing 'Through'
+        devices = [i for i in result.stdout.decode('utf-8').split('\n') if 'Through' not in i]
+        # find id and name for each device
+        self.devices = []
+        for dev in devices:
+            match = re.match("client (\d*)\: '(.*)'", dev)
+            if match:
+                self.devices.append({'id': match.group(1), 'name': match.group(2)})
+        return self.devices
 
 
-def getIp():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
-    return sock.getsockname()[0]
+class usbMidiHostUi():
+    buttons = [{'points': [(127, 20), (90, 40)], 'key': KEY1_PIN, 'text': 'in'},
+               {'points': [(127, 50), (90, 70)], 'key': KEY2_PIN, 'text': 'out'},
+               {'points': [(127, 80), (90, 100)], 'key': KEY3_PIN, 'text': 'sys'}]
+    # devices
+    connectedDevices = []
+    currentDeviceLeft = 0
+    currentDeviceRight = 0
+    currentSide = LEFT
+    # info
+    currentInfo = 0
+    pins = [KEY_UP_PIN,
+            KEY_DOWN_PIN,
+            KEY_LEFT_PIN,
+            KEY_RIGHT_PIN,
+            KEY_PRESS_PIN,
+            KEY1_PIN,
+            KEY2_PIN,
+            KEY3_PIN]
+
+    def __init__(self):
+        self.usbMidiHost = usbMidiHost()
+        self.infos = [f"status", f"ip:{self.getIp()}", f"uptime:{self.get_uptime()}", f'cpu:{psutil.cpu_percent()}%', f'ram:{psutil.virtual_memory().percent}%', 'creator: lukn303']
+        # init GPIO
+        GPIO.setmode(GPIO.BCM)
+        for pin in self.pins:
+            GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            GPIO.add_event_detect(pin, GPIO.FALLING,
+                                  callback=self.pinHandler,
+                                  bouncetime=300)
+
+        # 240x240 display with hardware SPI:
+        self.disp = display.LCD()
+        Lcd_ScanDir = display.SCAN_DIR_DFT  # SCAN_DIR_DFT = D2U_L2R
+        self.disp.LCD_Init(Lcd_ScanDir)
+        self.disp.LCD_Clear()
+
+        # Create blank image for drawing.
+        # Make sure to create image with mode '1' for 1-bit color.
+        width = 128
+        height = 128
+        self.image = Image.new('RGB', (width, height))
+
+        # Get drawing object to draw on image.
+        self.draw = ImageDraw.Draw(self.image)
+
+        # Draw a black filled box to clear the image.
+        self.draw.rectangle((0, 0, width, height), outline=0, fill=0)
+        self.disp.LCD_ShowImage(self.image, 0, 0)
+
+    def pinHandler(self, pin):
+        if pin == KEY_DOWN_PIN:
+            print("up")
+            if self.currentSide == LEFT:
+                self.currentDeviceLeft = self.decrease(self.currentDeviceLeft, len(self.usbMidiHost.getDeviceList()))
+            else:
+                self.currentDeviceRight = self.decrease(self.currentDeviceRight, len(self.usbMidiHost.getDeviceList()))
+        if pin == KEY_UP_PIN:
+            print("down")
+            if self.currentSide == LEFT:
+                self.currentDeviceLeft = self.increase(self.currentDeviceLeft, len(self.usbMidiHost.getDeviceList()))
+            else:
+                self.currentDeviceRight = self.increase(self.currentDeviceRight, len(self.usbMidiHost.getDeviceList()))
+
+        if pin in [KEY_LEFT_PIN, KEY_RIGHT_PIN, KEY_PRESS_PIN]:
+            self.currentSide = not self.currentSide
+
+        # Key1: switch informations
+        if pin == KEY1_PIN:
+            print("Key1")
+            self.currentInfo = self.currentInfo + 1 if self.currentInfo < len(self.infos) - 1 else 0
+        # Key2: Connect Currently selected devices
+        if pin == KEY2_PIN:
+            print("Key2")
+            if {'in': self.currentDeviceLeft, 'out': self.currentDeviceRight} in self.connectedDevices:
+                self.statusMessage("nothing to do")
+            else:
+                self.connectedDevices.append({'in': self.currentDeviceLeft, 'out': self.currentDeviceRight})
+                self.statusMessage(f"connected({self.currentDeviceLeft},{self.currentDeviceRight})")
+        # Key3: Disconnect Currently selected devices
+        if pin == KEY3_PIN:
+            print("Key2")
+            if {'in': self.currentDeviceLeft, 'out': self.currentDeviceRight} in self.connectedDevices:
+                self.connectedDevices.remove({'in': self.currentDeviceLeft, 'out': self.currentDeviceRight})
+                self.statusMessage(f"disconnected({self.currentDeviceLeft},{self.currentDeviceRight})")
+            else:
+                self.statusMessage("nothing to do")
+
+    @staticmethod
+    def midpoint(p1, p2):
+        return (int((p1[0]+p2[0])/2), int((p1[1]+p2[1])/2))
+
+    @staticmethod
+    def getTextPos(p1, p2):
+        return (p2[0] + 2, p1[1] + 2)
+
+    def drawDevices(self):
+        # left
+        for i, dev in enumerate(self.usbMidiHost.getDeviceList()):
+            self.draw.rectangle([(0, (i+1)*20), (40, (i+2)*20)], outline="red", fill=0)
+            self.draw.text((0, (i+1)*20), dev['name'][0:6], font=font)
+        # right
+        for i, dev in enumerate(self.usbMidiHost.getDeviceList()):
+            self.draw.rectangle([(87, (i+1)*20), (127, (i+2)*20)], outline="red", fill=0)
+            self.draw.text((87, (i+1)*20), dev['name'][0:6], font=font)
+
+    def drawInformations(self):
+        if self.currentInfo != 0:
+            self.infos = [f"status", f"ip:{self.getIp()}", f"uptime:{self.get_uptime()}", f'cpu:{psutil.cpu_percent()}%', f'ram:{psutil.virtual_memory().percent}%', 'creator: lukn303']
+        self.draw.rectangle([(0,0), (127, 15)], outline='red', fill=0)
+        self.draw.text((5, 0), self.infos[self.currentInfo], font=font)
+
+    @staticmethod
+    def get_uptime():
+        with open('/proc/uptime', 'r') as f:
+            seconds = float(f.readline().split()[0])
+        return str(timedelta(seconds=seconds)).split('.', maxsplit=1)[0]
+
+    @staticmethod
+    def getIp():
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
+        return sock.getsockname()[0]
+
+    def statusMessage(self, text):
+        self.infos[0] = f"{text}"
+        self.currentInfo = 0
+
+    def linedashed(self, x0, y0, x1, y1, dashlen=4, ratio=3):
+        # get deltas
+        deltax = x1 - x0
+        deltay = y1 - y0
+        # check whether we can avoid sqrt
+        if deltay == 0:
+            length = deltax
+        elif deltax == 0:
+            length = deltay
+        else:
+            # length of line
+            length = math.sqrt(deltax*deltax+deltay*deltay)
+        # x add for 1px line length
+        xa = deltax / length
+        # y add for 1px line length
+        ya = deltay / length
+        # step to the next dash
+        step = dashlen * ratio
+        a0 = 0
+        while a0 < length:
+            a1 = a0 + dashlen
+            a1 = min(a1, length)
+            self.draw.line((x0+xa*a0, y0+ya*a0, x0+xa*a1, y0+ya*a1))
+            a0 += step
+
+    def drawLines(self):
+        # draw cursor line with a dot on the current Side
+        p1 = (40, (self.currentDeviceLeft + 1) * 20 + 10)
+        p2 = (87, (self.currentDeviceRight + 1) * 20 + 10)
+        if self.currentSide == LEFT:
+            self.draw.ellipse((p1[0], p1[1] - 3, p1[0] + 6, p1[1] + 3), fill='white', outline='white')
+        else:
+            self.draw.ellipse((p2[0] - 6, p2[1] - 3, p2[0], p2[1] + 3), fill='white', outline='white')
+        self.linedashed(p1[0], p1[1], p2[0], p2[1])
+
+        # draw solid lines for connected Devices
+        for connected in self.connectedDevices:
+            p1 = (40, (connected['in'] + 1) * 20 + 10)
+            p2 = (87, (connected['out'] + 1) * 20 + 10)
+            self.draw.line((p1, p2))
+
+    @staticmethod
+    def decrease(num, maxNum):
+        return num - 1 if num > 0 else maxNum
+
+    @staticmethod
+    def increase(num, maxNum):
+        return num + 1 if num < maxNum - 1 else 0
+
+    def drawUi(self):
+        self.draw.rectangle([(0, 0), (127, 127)], fill=0)
+        self.drawInformations()
+        self.drawDevices()
+        self.drawLines()
+        self.disp.LCD_ShowImage(self.image, 0, 0)
 
 
-display = display.Display()
-print(getDeviceList())
-display.lcd_string(f"IP:{getIp()}", 0)
-for dev in getDeviceList():
-    display.lcd_string(dev['id'] + ':' + dev['name'], 1)
+if __name__ == "__main__":
+    ui = usbMidiHostUi()
+    try:
+        while 1:
+            ui.drawUi()
+    except Exception as e:
+        GPIO.cleanup()
+        raise e
